3c57d55ccd808545fc214ed062145cb3bfd5fdee
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-07 23:46:29 -0500

    Add Test [Xpath]

---
+++ src/test/java/org/locators/By_xpath.java
@@ -0,0 +1,29 @@
+package org.locators;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class By_xpath {
+
+    public static void main(String[] args) {
+        // Configurar el WebDriver de Chrome usando WebDriverManager
+        WebDriverManager.chromedriver().driverVersion("117.0.5938.149").setup();
+
+        // Crear una instancia de WebDriver para Chrome
+        WebDriver driver = new ChromeDriver();
+
+        // Realizar tus tareas de automatización de Selenium utilizando la instancia 'driver'
+        driver.manage().window().maximize();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.xpath("//input[@id=\"user-name\"]")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+
+        // Recuerda cerrar la instancia de WebDriver cuando hayas terminado
+        driver.quit();
+
+    }
+
+}
